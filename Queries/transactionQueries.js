36e6dc048625d29a00cf3de53ca2fdb3f05e7dd7
Tanmay Sharma <sharmatanmay617@gmail.com>
2023-04-30 14:13:03 +0300

    Some changes made

--- Queries/transactionQueries.js
+++ Queries/transactionQueries.js
@@ -1,280 +1,280 @@
-require('dotenv').config();
-const config = require('../utils/config');
+// require('dotenv').config();
+// const config = require('../utils/config');
 
-const Pool = require('pg').Pool;
+// const Pool = require('pg').Pool;
 
-const pool = new Pool({
-    user: config.PG_USER,
-    host: config.PG_HOST,
-    database: config.PG_DATABASE,
-    password: config.PG_PASSWORD,
-    port: config.PG_PORT,
-});
-
-
-
-const sendAmountToAnotherUserInternally = async (req, res) => {
-
-    const body = req.body;
-
-    const {
-        amount,
-        source_account_number,
-        destination_account_number
-    } = body;
-
-    if (!(amount && source_account_number && destination_account_number)) {
-        res.status(400).send("All input is required.");
-        return;
-    }
-
-    let result;
-
-    // currently we have the same currency
-    // everything including transactions
-    const transactionPromise = new Promise(async (resolve, reject) => {
-
-        // we use the basic concept of transactions
-        // we need begin and rollback as we are carrying
-        // out many transactions in one query.
-        pool.query("BEGIN;")
-            .then((results) => {
-                return pool.query(
-                    "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
-                );
-            })
-            .then((results) => {
-            return pool.query(
-                "UPDATE accounts SET balance=(balance-$1) WHERE account_number=$2;",
-                [amount, source_account_number]
-            );
-            })
-            .then((results) => {
-                return pool.query(
-                    'Update accounts set balance=(balance+$1) where account_number=$2;',
-                    [amount, destination_account_number]
-                );
-            })
-            .then((results) => {
-                return pool.query("COMMIT;");
-            })
-            .then((results) => {
-                console.log('transaction completed');
-            })
-            .catch((err) => {
-                console.error('error while querying:', err);
-                return pool.query('rollback')
-            })
-            .catch((err) => {
-                console.error('error while rolling back transaction:', err);
-            })
-
-        result = await storeInternalTransactions({
-            source_account_number: source_account_number,
-            destination_account_number: destination_account_number,
-            transaction_type: 'TRANSFER',
-            transaction_amount: amount
-        });
-
-        resolve("Transaction Completed!");
-    });
-
-    await transactionPromise;
-
-    res.status(201).send(result);
-
-};
-
-const depositToAccount = async (req, res) => {
-    const body = req.body;
-
-    const {
-        amount,
-        account_number
-    } = body;
-
-    if(!(amount && account_number)) {
-        res.status(400).send("All input is required.");
-        return;
-    }
-
-    let result;
-
-
-    const transactionPromise = new Promise(async (resolve, reject) => {
-        pool.query("BEGIN;")
-            .then((results) => {
-                return pool.query(
-                    "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
-                );
-            })
-            .then((results) => {
-                return pool.query('Update accounts set balance=(balance+$1) where account_number=$2;', [amount, account_number], (error, results) => {
-                    if (error) {
-                        reject(error);
-                        throw error;
-                    }
-                });
-            })
-            .then((results) => {
-                return pool.query("COMMIT;");
-            })
-            .then((results) => {
-                console.log('transaction completed');
-            })
-            .catch((err) => {
-                console.error('error while querying:', err);
-                return pool.query('rollback')
-            })
-            .catch((err) => {
-                console.error('error while rolling back transaction:', err);
-            })
-
-        result = await storeInternalTransactions({
-            source_account_number: null,
-            destination_account_number: account_number,
-            transaction_type: 'DEPOSIT',
-            transaction_amount: amount
-        });
-
-        resolve(result);
-    });
-
-    await transactionPromise;
-
-    res.status(201).send(result);
-};
-
-const withdrawalFromAccount = async (req, res) => {
-    const body = req.body;
-
-    const {
-        amount,
-        account_number
-    } = body;
-
-    if(!(amount && account_number)) {
-        res.status(400).send("All input is required.");
-        return;
-    }
-
-    let result;
-
-    const transactionPromise = new Promise(async (resolve, reject) => {
-        pool.query("BEGIN;")
-            .then((results) => {
-                return pool.query(
-                    "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
-                );
-            })
-            .then((results) => {
-                return pool.query('Update accounts set amount=(amount-$1) where account_number=$2;', [amount, account_number], (error, results) => {
-                    if (error) {
-                        reject(error);
-                        throw error;
-                    }
-                });
-            })
-            .then((results) => {
-                return pool.query("COMMIT;");
-            })
-            .then((results) => {
-                console.log('transaction completed');
-            })
-            .catch((err) => {
-                console.error('error while querying:', err);
-                return pool.query('rollback')
-            })
-            .catch((err) => {
-                console.error('error while rolling back transaction:', err);
-            })
-
-        result = await storeInternalTransactions({
-            source_account_number: null,
-            destination_account_number: account_number,
-            transaction_type: 'WITHDRAWAL',
-            transaction_amount: amount
-        });
-
-        resolve(result);
-    });
-
-    await transactionPromise;
-
-    res.status(201).send(result);
-}
-
-
-const storeInternalTransactions = async (params) => {
-
-    const {
-        source_account_number,
-        destination_account_number,
-        transaction_type,
-        transaction_amount
-    } = params;
+// const pool = new Pool({
+//     user: config.PG_USER,
+//     host: config.PG_HOST,
+//     database: config.PG_DATABASE,
+//     password: config.PG_PASSWORD,
+//     port: config.PG_PORT,
+// });
 
 
 
-    let transactionID;
+// const sendAmountToAnotherUserInternally = async (req, res) => {
 
-    // retrieving the transaction id
-    const transactionIDPromise = new Promise((resolve, reject) => {
-        pool.query("BEGIN;")
-            .then((results) => {
-                return pool.query(
-                    "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
-                );
-            })
-            .then((results) => {
-                return pool.query('select max(id) from internal_transactions;', (error, results) => {
-                    if (error) {
-                        reject(error);
-                        throw error;
-                    }
+//     const body = req.body;
 
-                    if (results.rows.length === 0) {
-                        transactionID = 1;
-                    } else {
-                        transactionID = results.rows[0].max + 1;
-                    }
+//     const {
+//         amount,
+//         source_account_number,
+//         destination_account_number
+//     } = body;
 
-                    resolve(transactionID);
-                })
-            })
-            .then((results) => {
-                return pool.query("COMMIT;");
-            })
-            .then((results) => {
-                console.log('transaction completed');
-            })
-            .catch((err) => {
-                console.error('error while querying:', err);
-                return pool.query('rollback')
-            })
-            .catch((err) => {
-                console.error('error while rolling back transaction:', err);
-            })
-    });
+//     if (!(amount && source_account_number && destination_account_number)) {
+//         res.status(400).send("All input is required.");
+//         return;
+//     }
 
-    await transactionIDPromise;
+//     let result;
 
-    return new Promise((resolve, reject) => {
-        pool.query('Insert into internal_transactions(id, source_account_number, destination_account_number, transaction_type, transaction_amount) values($1, $2, $3, $4, $5);', [transactionID, source_account_number, destination_account_number, transaction_type, transaction_amount], (error, results) => {
-            if (error) {
-                reject(error);
-                throw error;
-            }
+//     // currently we have the same currency
+//     // everything including transactions
+//     const transactionPromise = new Promise(async (resolve, reject) => {
 
-            resolve(`Transaction record added with id:${transactionID}`);
-        });
+//         // we use the basic concept of transactions
+//         // we need begin and rollback as we are carrying
+//         // out many transactions in one query.
+//         pool.query("BEGIN;")
+//             .then((results) => {
+//                 return pool.query(
+//                     "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
+//                 );
+//             })
+//             .then((results) => {
+//             return pool.query(
+//                 "UPDATE accounts SET balance=(balance-$1) WHERE account_number=$2;",
+//                 [amount, source_account_number]
+//             );
+//             })
+//             .then((results) => {
+//                 return pool.query(
+//                     'Update accounts set balance=(balance+$1) where account_number=$2;',
+//                     [amount, destination_account_number]
+//                 );
+//             })
+//             .then((results) => {
+//                 return pool.query("COMMIT;");
+//             })
+//             .then((results) => {
+//                 console.log('transaction completed');
+//             })
+//             .catch((err) => {
+//                 console.error('error while querying:', err);
+//                 return pool.query('rollback')
+//             })
+//             .catch((err) => {
+//                 console.error('error while rolling back transaction:', err);
+//             })
 
-    });
-}
+//         result = await storeInternalTransactions({
+//             source_account_number: source_account_number,
+//             destination_account_number: destination_account_number,
+//             transaction_type: 'TRANSFER',
+//             transaction_amount: amount
+//         });
+
+//         resolve("Transaction Completed!");
+//     });
+
+//     await transactionPromise;
+
+//     res.status(201).send(result);
+
+// };
+
+// const depositToAccount = async (req, res) => {
+//     const body = req.body;
+
+//     const {
+//         amount,
+//         account_number
+//     } = body;
+
+//     if(!(amount && account_number)) {
+//         res.status(400).send("All input is required.");
+//         return;
+//     }
+
+//     let result;
 
 
-module.exports = {
-    sendAmountToAnotherUserInternally,
-    depositToAccount,
-    withdrawalFromAccount
-}
+//     const transactionPromise = new Promise(async (resolve, reject) => {
+//         pool.query("BEGIN;")
+//             .then((results) => {
+//                 return pool.query(
+//                     "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
+//                 );
+//             })
+//             .then((results) => {
+//                 return pool.query('Update accounts set balance=(balance+$1) where account_number=$2;', [amount, account_number], (error, results) => {
+//                     if (error) {
+//                         reject(error);
+//                         throw error;
+//                     }
+//                 });
+//             })
+//             .then((results) => {
+//                 return pool.query("COMMIT;");
+//             })
+//             .then((results) => {
+//                 console.log('transaction completed');
+//             })
+//             .catch((err) => {
+//                 console.error('error while querying:', err);
+//                 return pool.query('rollback')
+//             })
+//             .catch((err) => {
+//                 console.error('error while rolling back transaction:', err);
+//             })
+
+//         result = await storeInternalTransactions({
+//             source_account_number: null,
+//             destination_account_number: account_number,
+//             transaction_type: 'DEPOSIT',
+//             transaction_amount: amount
+//         });
+
+//         resolve(result);
+//     });
+
+//     await transactionPromise;
+
+//     res.status(201).send(result);
+// };
+
+// const withdrawalFromAccount = async (req, res) => {
+//     const body = req.body;
+
+//     const {
+//         amount,
+//         account_number
+//     } = body;
+
+//     if(!(amount && account_number)) {
+//         res.status(400).send("All input is required.");
+//         return;
+//     }
+
+//     let result;
+
+//     const transactionPromise = new Promise(async (resolve, reject) => {
+//         pool.query("BEGIN;")
+//             .then((results) => {
+//                 return pool.query(
+//                     "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
+//                 );
+//             })
+//             .then((results) => {
+//                 return pool.query('Update accounts set amount=(amount-$1) where account_number=$2;', [amount, account_number], (error, results) => {
+//                     if (error) {
+//                         reject(error);
+//                         throw error;
+//                     }
+//                 });
+//             })
+//             .then((results) => {
+//                 return pool.query("COMMIT;");
+//             })
+//             .then((results) => {
+//                 console.log('transaction completed');
+//             })
+//             .catch((err) => {
+//                 console.error('error while querying:', err);
+//                 return pool.query('rollback')
+//             })
+//             .catch((err) => {
+//                 console.error('error while rolling back transaction:', err);
+//             })
+
+//         result = await storeInternalTransactions({
+//             source_account_number: null,
+//             destination_account_number: account_number,
+//             transaction_type: 'WITHDRAWAL',
+//             transaction_amount: amount
+//         });
+
+//         resolve(result);
+//     });
+
+//     await transactionPromise;
+
+//     res.status(201).send(result);
+// }
+
+
+// const storeInternalTransactions = async (params) => {
+
+//     const {
+//         source_account_number,
+//         destination_account_number,
+//         transaction_type,
+//         transaction_amount
+//     } = params;
+
+
+
+//     let transactionID;
+
+//     // retrieving the transaction id
+//     const transactionIDPromise = new Promise((resolve, reject) => {
+//         pool.query("BEGIN;")
+//             .then((results) => {
+//                 return pool.query(
+//                     "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;"
+//                 );
+//             })
+//             .then((results) => {
+//                 return pool.query('select max(id) from internal_transactions;', (error, results) => {
+//                     if (error) {
+//                         reject(error);
+//                         throw error;
+//                     }
+
+//                     if (results.rows.length === 0) {
+//                         transactionID = 1;
+//                     } else {
+//                         transactionID = results.rows[0].max + 1;
+//                     }
+
+//                     resolve(transactionID);
+//                 })
+//             })
+//             .then((results) => {
+//                 return pool.query("COMMIT;");
+//             })
+//             .then((results) => {
+//                 console.log('transaction completed');
+//             })
+//             .catch((err) => {
+//                 console.error('error while querying:', err);
+//                 return pool.query('rollback')
+//             })
+//             .catch((err) => {
+//                 console.error('error while rolling back transaction:', err);
+//             })
+//     });
+
+//     await transactionIDPromise;
+
+//     return new Promise((resolve, reject) => {
+//         pool.query('Insert into internal_transactions(id, source_account_number, destination_account_number, transaction_type, transaction_amount) values($1, $2, $3, $4, $5);', [transactionID, source_account_number, destination_account_number, transaction_type, transaction_amount], (error, results) => {
+//             if (error) {
+//                 reject(error);
+//                 throw error;
+//             }
+
+//             resolve(`Transaction record added with id:${transactionID}`);
+//         });
+
+//     });
+// }
+
+
+// module.exports = {
+//     sendAmountToAnotherUserInternally,
+//     depositToAccount,
+//     withdrawalFromAccount
+// }
