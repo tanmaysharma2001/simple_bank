83be7dfc0044d6d1fa2fdfff00ce9e790362b1c5
Tanmay Sharma <sharmatanmay617@gmail.com>
2023-04-30 22:58:23 +0300

    Changes made

--- migrations/initDB.js
+++ migrations/initDB.js
@@ -14,7 +14,7 @@ const pool = new Pool({
 const createTableUsers = () => {
     pool.query('create table users(id serial unique not null, first_name varchar(50), last_name varchar(50), email varchar(50) unique not null, password varchar(100), token varchar(200), account_number varchar(30) unique not null, PRIMARY KEY (id, email, account_number));', (error, results) => {
         if (error) {
-            throw results;
+            throw error;
         }
     });
 }
