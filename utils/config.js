3ee8fdd220081cda32ef28e853e0f93501cfdd52
Tanmay Sharma <sharmatanmay617@gmail.com>
2023-04-30 14:18:52 +0300

    Some changes made

--- utils/config.js
+++ utils/config.js
@@ -6,7 +6,7 @@ const TOKEN_KEY = process.env.TOKEN_KEY;
 
 // Postgres
 const PG_HOST = process.env.PG_HOST;
-const PG_PORT = process.env.NODE_ENV === 'test' ? 2345 : Number(process.env.PG_HOST);
+const PG_PORT = process.env.NODE_ENV === 'test' ? 5432 : Number(process.env.PG_PORT);
 const PG_USER = process.env.PG_USER;
 const PG_PASSWORD = process.env.PG_PASSWORD;
 const PG_DATABASE = process.env.PG_DATABASE;
