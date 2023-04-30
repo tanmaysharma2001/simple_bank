f68cb44ef603a55a7b75e5eb69a846b6530410b8
Tanmay Sharma <sharmatanmay617@gmail.com>
2023-05-01 00:02:25 +0300

    More tables added

--- migrations/initDB.js
+++ migrations/initDB.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const { resolve } = require('path');
 const config = require('../utils/config');
+const { reject } = require('lodash');
 
 const Pool = require('pg').Pool;
 
@@ -12,42 +14,54 @@ const pool = new Pool({
 });
 
 
-const createTableUsers = () => {
+const createTableUsersPromise = new Promise(async (resolve, reject) => {
     pool.query('create table users(id serial unique not null, first_name varchar(50), last_name varchar(50), email varchar(50) unique not null, password varchar(100), token varchar(200), account_number varchar(30) unique not null, PRIMARY KEY (id, email, account_number));', (error, results) => {
         if (error) {
             throw error;
         }
     });
-};
 
-const createTableAccounts = () => {
+    resolve("User Table Created!");
+});
+
+const createTableAccountsPromise = new Promise(async(resolve, reject) => {
     pool.query('create table accounts(account_number varchar(50) primary key, account_type varchar(30) not null, balance numeric(10, 2) not null);', (error, results) => {
         if (error) {
             throw error;
         }
     });
-};
 
-const createTableInternalTransactions = () => {
-    pool.query('create table internal_transactions(id serial not null, source_account_number varchar(50), destination_account_number varchar(50) not null, transaction_type VARCHAR(20) NOT NULL, transaction_amount NUMERIC(10, 2) NOT NULL, transaction_date           TIMESTAMP NOT NULL DEFAULT NOW(), PRIMARY KEY (id), FOREIGN KEY (source_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE, FOREIGN KEY (destination_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE);', (error, results) => {
+    resolve("Accounts table created!");
+});
+
+
+
+const createTableInternalTransactionsPromise = new Promise(async(resolve, reject) => {
+    pool.query('create table internal_transactions(id serial not null, source_account_number varchar(50), destination_account_number varchar(50) not null, transaction_type VARCHAR(20) NOT NULL, transaction_amount NUMERIC(10, 2) NOT NULL, transaction_date TIMESTAMP NOT NULL DEFAULT NOW(), PRIMARY KEY (id), FOREIGN KEY (source_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE, FOREIGN KEY (destination_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE);', (error, results) => {
         if (error) {
             throw error;
         }
     });
-};
 
-const createTableExternalTransactions = () => {
+    resolve("Internal Transactions Table Created!");
+});
+
+
+
+const createTableExternalTransactionsPromise = new Promise(async(resolve, reject) => {
     pool.query('CREATE TABLE external_transactions(id SERIAL PRIMARY KEY, source_account_number VARCHAR(50) NOT NULL, destination_bank_name VARCHAR(100) NOT NULL, destination_account_number VARCHAR(50) NOT NULL, transaction_type VARCHAR(20) NOT NULL, transaction_amount NUMERIC(10, 2) NOT NULL, transaction_date TIMESTAMP NOT NULL DEFAULT NOW(), FOREIGN KEY (source_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE);', (error, results) => {
         if (error) {
             throw error;
         }
     });
-};
 
-createTableUsers();
+    resolve("External Transactions Created!");
+});
 
-createTableAccounts();
+await createTableUsersPromise();
 
-createTableInternalTransactions();
+await createTableAccountsPromise();
 
-createTableExternalTransactions();
+await createTableInternalTransactionsPromise();
+
+await createTableExternalTransactionsPromise();
