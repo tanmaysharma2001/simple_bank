12a1d36273b34a0a3f2a083d99143ea37f94c11c
Tanmay Sharma <sharmatanmay617@gmail.com>
2023-05-01 00:16:40 +0300

    Some changes made

--- migrations/initDB.js
+++ migrations/initDB.js
@@ -10,7 +10,7 @@ const pool = new Pool({
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    port: process.env.NODE_ENV === 'test' ? '2345' : process.env.PG_PORT,
 });
 
 
