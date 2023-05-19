bca07251569d0772c6990960dfde9d9d865a3209
tanmaysharma2001 <sharmatanmay617@gmail.com>
2023-05-19 16:32:07 +0300

    initDb.js modified

--- migrations/initDB.js
+++ migrations/initDB.js
@@ -63,6 +63,8 @@ const createAllTables = async () => {
 
     await createTableAccountsPromise;
 
+    await new Promise(r => setTimeout(r, 2000));
+
     await createTableInternalTransactionsPromise;
 
     await createTableExternalTransactionsPromise;
