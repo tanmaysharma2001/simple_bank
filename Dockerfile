a5794ca6680294be8bbcf006fed2719b09f0e56b
Tanmay Sharma <sharmatanmay617@gmail.com>
2023-10-25 14:48:12 +0300

    Some changes

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,38 @@
-FROM node:14-alpine
+# use the official Bun image
+# see all versions at https://hub.docker.com/r/oven/bun/tags
+FROM oven/bun:1 as base
+WORKDIR /usr/src/app
 
-WORKDIR /app
+# install dependencies into temp directory
+# this will cache them and speed up future builds
+FROM base AS install
+RUN mkdir -p /temp/dev
+COPY package.json bun.lockb /temp/dev/
+RUN cd /temp/dev && bun install --frozen-lockfile
 
-COPY package.json .
+# install with --production (exclude devDependencies)
+RUN mkdir -p /temp/prod
+COPY package.json bun.lockb /temp/prod/
+RUN cd /temp/prod && bun install --frozen-lockfile --production
 
-COPY package-lock.json .
-
-RUN npm install --only=production
-RUN npm install --save-dev typescript
-
+# copy node_modules from temp directory
+# then copy all (non-ignored) project files into the image
+FROM install AS prerelease
+COPY --from=install /temp/dev/node_modules node_modules
 COPY . .
 
-#ENV NODE_ENV=development
-#ENV DATABASE_URL=postgres://me:google12@postgres:5432/simple_bank
+# [optional] tests & build
+ENV NODE_ENV=production
+RUN bun test
+RUN bun run build
 
-EXPOSE 8080
+# copy production dependencies and source code into final image
+FROM base AS release
+COPY --from=install /temp/prod/node_modules node_modules
+COPY --from=prerelease /usr/src/app/index.ts .
+COPY --from=prerelease /usr/src/app/package.json .
 
-CMD ["npm", "start"]
+# run the app
+USER bun
+EXPOSE 3000/tcp
+ENTRYPOINT [ "bun", "run", "index.ts" ]
---
+++ migrations/initDB.ts
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const { resolve } = require('path');
+import config from "../utils/config";
+// import {reject} from 'lodash';
+// const { reject } = require('lodash');
+
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.NODE_ENV === 'test' ? '2345' : process.env.PG_PORT,
+});
+
+
+const createTableUsersPromise = new Promise(async (resolve, reject) => {
+    pool.query('create table users(id serial unique not null, first_name varchar(50), last_name varchar(50), email varchar(50) unique not null, password varchar(100), token varchar(200), account_number varchar(30) unique not null, PRIMARY KEY (id, email, account_number));', (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+    });
+
+    resolve("User Table Created!");
+});
+
+const createTableAccountsPromise = new Promise(async(resolve, reject) => {
+    pool.query('create table accounts(account_number varchar(50) primary key, account_type varchar(30) not null, balance numeric(10, 2) not null);', (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+    });
+
+    resolve("Accounts table created!");
+});
+
+
+
+const createTableInternalTransactionsPromise = new Promise(async(resolve, reject) => {
+    pool.query('create table internal_transactions(id serial not null, source_account_number varchar(50), destination_account_number varchar(50) not null, transaction_type VARCHAR(20) NOT NULL, transaction_amount NUMERIC(10, 2) NOT NULL, transaction_date TIMESTAMP NOT NULL DEFAULT NOW(), PRIMARY KEY (id), FOREIGN KEY (source_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE, FOREIGN KEY (destination_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE);', (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+    });
+
+    resolve("Internal Transactions Table Created!");
+});
+
+
+
+const createTableExternalTransactionsPromise = new Promise(async(resolve, reject) => {
+    pool.query('CREATE TABLE external_transactions(id SERIAL PRIMARY KEY, source_account_number VARCHAR(50) NOT NULL, destination_bank_name VARCHAR(100) NOT NULL, destination_account_number VARCHAR(50) NOT NULL, transaction_type VARCHAR(20) NOT NULL, transaction_amount NUMERIC(10, 2) NOT NULL, transaction_date TIMESTAMP NOT NULL DEFAULT NOW(), FOREIGN KEY (source_account_number) REFERENCES accounts (account_number) ON DELETE CASCADE);', (error: any, results: any) => {
+        if (error) {
+            throw error;
+        }
+    });
+
+    resolve("External Transactions Created!");
+});
+
+const createAllTables = async () => {
+    await createTableUsersPromise;
+
+    await createTableAccountsPromise;
+
+    await new Promise(r => setTimeout(r, 4000));
+
+    await createTableInternalTransactionsPromise;
+
+    await createTableExternalTransactionsPromise;
+};
+
+createAllTables();
